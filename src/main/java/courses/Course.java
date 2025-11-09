ed3740b29a3325a25138921ac10103aa520ed028
Andrzej Korbiel <andkorbiel@gmail.com>
2025-11-09 16:27:52 +0100

    refactoring

--- src/main/java/courses/Course.java
+++ src/main/java/courses/Course.java
@@ -1,20 +1,11 @@
 package courses;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import students.Student;
-
 public class Course {
-  private int availableSeats;
   private String description;
-  private Map<Integer, Student> enrolledStudents = new HashMap<>();
-  private boolean isCourseFull = this.availableSeats == 0;
+  private Double hoursPerWeek;
   private String name;
   private String teacherName;
-  private List<Student> waitingStudentsList = new LinkedList<>();
+  public CourseParticipants courseParticipatns;
 
   public Course(
       String description,
@@ -24,7 +15,7 @@ public class Course {
     this.description = description;
     this.name = name;
     this.teacherName = teacherName;
-    this.availableSeats = availableSeats;
+    this.courseParticipatns = new CourseParticipants(availableSeats);
   }
 
   public String getDescription() {
@@ -39,41 +30,7 @@ public class Course {
     return this.teacherName;
   }
 
-  public int getAvailableSeats() {
-    return this.availableSeats;
-  }
-
-  public Map<Integer, Student> getEnrolledStudentsList() {
-    return this.enrolledStudents;
-  }
-
-  public boolean checkIfCourseIsFull() {
-    return this.isCourseFull;
-  }
-
-  public void enrollStudent(Student newStudent) {
-    if (this.checkIfCourseIsFull()) {
-      int studentId = newStudent.getId();
-
-      this.enrolledStudents.put(studentId, newStudent);
-      this.availableSeats--;
-    } else {
-      this.waitingStudentsList.add(newStudent);
-    }
-  }
-
-  public void removeStudent(Student currentStudent) {
-    int studentId = currentStudent.getId();
-
-    this.enrolledStudents.remove(studentId);
-
-    if (!this.waitingStudentsList.isEmpty()) {
-      Student studentFromQueue = this.waitingStudentsList.getFirst();
-
-      this.enrollStudent(studentFromQueue);
-      this.waitingStudentsList.removeFirst();
-    } else {
-      this.availableSeats++;
-    }
+  public Double getHoursePerWeek() {
+    return this.hoursPerWeek;
   }
 }
--- src/test/java/courses/CourseTest.java
+++ src/test/java/courses/CourseTest.java
@@ -1,14 +1,7 @@
 package courses;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Test;
-
-import students.Student;
 
 public class CourseTest {
   // given
@@ -18,74 +11,12 @@ public class CourseTest {
   int availableSeats = 10;
 
   private final Course testCourse = new Course(description, name, teacherName, 10);
-  private final Student studentA = new Student("John");
-  private final Student studentB = new Student("Peter");
 
   @Test
   public void testCourseConstructor() {
     assertEquals(name, testCourse.getName());
     assertEquals(description, testCourse.getDescription());
     assertEquals(teacherName, testCourse.getTeacherName());
-    assertEquals(10, testCourse.getAvailableSeats());
-  }
-
-  @Test
-  public void testEnrollStudents() {
-    // given
-    final Map<Integer, Student> enrolledStudents = new HashMap<>();
-
-    // when
-    assertEquals(10, testCourse.getAvailableSeats());
-    assertTrue(testCourse.getEnrolledStudentsList().isEmpty());
-
-    testCourse.enrollStudent(studentA);
-    enrolledStudents.put(studentA.getId(), studentA);
-
-    // then
-    assertEquals(9, testCourse.getAvailableSeats());
-    assertTrue(testCourse.getEnrolledStudentsList().equals(enrolledStudents));
-
-    // when
-    testCourse.enrollStudent(studentB);
-    enrolledStudents.put(studentB.getId(), studentB);
-
-    // then
-    assertEquals(8, testCourse.getAvailableSeats());
-    assertTrue(testCourse.getEnrolledStudentsList().equals(enrolledStudents));
-  }
-
-  @Test
-  public void testRemoveEnrolledStudents() {
-    // given
-    final Map<Integer, Student> enrolledStudents = new HashMap<>();
-
-    testCourse.enrollStudent(studentA);
-    testCourse.enrollStudent(studentB);
-    enrolledStudents.put(studentA.getId(), studentA);
-    enrolledStudents.put(studentB.getId(), studentB);
-
-    assertEquals(8, testCourse.getAvailableSeats());
-    assertTrue(testCourse.getEnrolledStudentsList().equals(enrolledStudents));
-
-    // when
-    testCourse.removeStudent(studentA);
-    enrolledStudents.remove(studentA.getId());
-
-    // then
-    assertEquals(9, testCourse.getAvailableSeats());
-    assertTrue(testCourse.getEnrolledStudentsList().equals(enrolledStudents));
-  }
-
-  @Test
-  public void testWaitingStudentsList() {
-    // given
-    final Course testCourseWithTwoSeats = new Course(description, name, teacherName, 2);
-
-    testCourseWithTwoSeats.enrollStudent(studentA);
-    testCourseWithTwoSeats.enrollStudent(studentB);
-
-    assertEquals(0, testCourseWithTwoSeats.getAvailableSeats());
-    assertEquals(2, testCourseWithTwoSeats.getEnrolledStudentsList().size());
-    assertEquals(true, testCourseWithTwoSeats.checkIfCourseIsFull());
+    assertEquals(10, testCourse.courseParticipatns.getAvailableSeats());
   }
 }
